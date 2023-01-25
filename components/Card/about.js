ccd3e7bc11525496d2ddbd9f4a8bee07693bba9c
johnathn.shang <johnathan.shang@gmail.com>
2023-01-24 20:39:23 -0800

    stylized pages

---
+++ components/Card/about.js
@@ -0,0 +1,41 @@
+export default function Card ({
+    firstName="none",
+    gpa="none",
+    graduated="none",
+    studentNumber="none",
+    font="0",
+    image="icon/personTwo.png"
+}) {
+    return(
+        <div style={{
+            backgroundColor: "darkblue", 
+            fontSize: font,
+            border: "2px solid",
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "30px",
+            width: "800px",
+            padding: "20px",
+            margin: "20px",
+            fontWeight: "800"
+        }}>
+            <div style={{
+                marginRight: "20px",
+                marginLeft: "20px"
+            }}>
+                {<img src={image}/>}
+            </div>
+            <div style={{
+                padding: "10px",
+                width: "10px",
+                color: "white",
+                textAlign: "justify",
+            }}>Student Number:{studentNumber}</div>
+            <div style={{
+                padding: "10px",
+                textAlign: "right",
+                width: "100%"
+            }}>Graduated:{graduated}</div>
+        </div>
+    )
+}
--- components/Card/index.js
+++ components/Card/index.js
@@ -1,18 +1,47 @@
+import { withRouter } from "next/router";
+
 export default function Card({
     degree = "none",
     colour = "purple",
     font = "0",
+    fontColour = "white",
     image = "icon/personTwo.png"
 }) {
     return(
-        <div style={{backgroundColor: colour, fontSize: font}}>
-            {
+        <div style={{
+            backgroundColor: colour, 
+            fontSize: font,
+            border: "2px solid",
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "30px",
+            width: "600px",
+            padding: "20px",
+            margin: "20px",
+            fontWeight: "800"
+        }}>
+
+            <div style={{
+                marginRight: "50px",
+                marginLeft: "10px"
+            }}>
+                {
                 colour == "red" ? <img src = {'icon/person.png'} width = '50' /> :
                 colour == "blue" ? <img src = {'icon/personThree.png'} width = '50' /> :
-                                            <img src = {image} width = '50' />
-                
-            }
-            {degree}
+                                            <img src = {image} width = '50' />    
+                }
+            </div>
+            <div style={{
+                backgroundColor: "darkBlue",
+                borderRadius: "20px",
+                padding: "20px",
+                display: 'flex',
+                flexDirection: "column",
+                justifyContent: "center",
+                width: "500px"
+            }}>
+                {degree}
+            </div>
         </div>
     )
 }
